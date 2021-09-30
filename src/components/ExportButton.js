2e032803db125fdb918dcb72b2c8b9d513c357dd
Val Lipat <vlipat.dev@gmail.com>
2021-09-30 18:27:29 +0800

    pre grid

--- src/components/ExportButton.js
+++ src/components/ExportButton.js
@@ -4,7 +4,7 @@ import DownloadIcon from '@mui/icons-material/Download';
 function ExportButton() {
 	return (
 		<Button
-			sx={{ margin: 1, minWidth: '250px' }}
+			sx={{ margin: 1, minWidth: '200px' }}
 			variant="contained"
 			color="success"
 			startIcon={<DownloadIcon />}
